7ecb4fdec6a9d099dba3109d2bc9ef3f60909308
0xIbuki <junta8918@gmail.com>
2025-11-05 11:12:22 +0900

    change to CreateAgentPosition event name

--- contracts/Logger.sol
+++ contracts/Logger.sol
@@ -3,11 +3,12 @@ pragma solidity =0.8.28;
 
 /**
  * @title Logger
- * @notice Logger contract for emitting rate arbitrage position events in Safe wallet batch transactions
+ * @notice Logger contract for emitting agent position events in Safe wallet batch transactions
  * @dev This contract can be included in Safe multi-sig batch transactions to emit tracking events
  */
 contract Logger {
-    event CreateRateArbitragePosition(
+    event CreateAgentPosition(
+        uint8 strategyType,
         address indexed user,
         address indexed collateralAsset,
         address indexed debtAsset,
@@ -15,10 +16,12 @@ contract Logger {
         address borrowMarketId,
         uint256 collateralAmount,
         uint256 debtAmount,
-        address supplyVault
+        address additionalInteractionContract,
+        bytes customData
     );
 
-    event CloseRateArbitragePosition(
+    event CloseAgentPosition(
+        uint8 strategyType,
         address indexed user,
         address indexed collateralAsset,
         address indexed debtAsset,
@@ -26,29 +29,35 @@ contract Logger {
         address borrowMarketId,
         uint256 collateralAmount,
         uint256 debtAmount,
-        address supplyVault
+        address additionalInteractionContract,
+        bytes customData
     );
 
     /**
-     * @notice Log rate arbitrage position creation
+     * @notice Log agent position creation
+     * @param strategyType The strategy type (0: rate arbitrage, 1: custom, etc)
      * @param collateralAsset The collateral asset address
      * @param debtAsset The debt asset address
      * @param borrowProtocol The borrowProtocol ID (0: AAVE_V3, 1: COMPOUND, 2: MORPHO, 3: FLUID, 4: MOONWELL)
      * @param borrowMarketId The market/vault address where debt is borrowed
      * @param collateralAmount The collateral amount supplied
      * @param debtAmount The debt amount borrowed
-     * @param supplyVault The vault/pool address where collateral is supplied
+     * @param additionalInteractionContract Additional contract address for interaction
+     * @param customData Arbitrary additional data for strategy-specific information
      */
-    function logCreateRateArbitragePosition(
+    function logCreateAgentPosition(
+        uint8 strategyType,
         address collateralAsset,
         address debtAsset,
         uint8 borrowProtocol,
         address borrowMarketId,
         uint256 collateralAmount,
         uint256 debtAmount,
-        address supplyVault
+        address additionalInteractionContract,
+        bytes calldata customData
     ) external {
-        emit CreateRateArbitragePosition(
+        emit CreateAgentPosition(
+            strategyType,
             msg.sender,
             collateralAsset,
             debtAsset,
@@ -56,30 +65,36 @@ contract Logger {
             borrowMarketId,
             collateralAmount,
             debtAmount,
-            supplyVault
+            additionalInteractionContract,
+            customData
         );
     }
 
     /**
-     * @notice Log rate arbitrage position close
+     * @notice Log agent position close
+     * @param strategyType The strategy type (0: rate arbitrage, 1: custom, etc)
      * @param collateralAsset The collateral asset address
      * @param debtAsset The debt asset address
      * @param borrowProtocol The borrowProtocol ID (0: AAVE_V3, 1: COMPOUND, 2: MORPHO, 3: FLUID, 4: MOONWELL)
      * @param borrowMarketId The market/vault address where debt was borrowed
      * @param collateralAmount The collateral amount withdrawn
      * @param debtAmount The debt amount repaid
-     * @param supplyVault The vault/pool address where collateral was supplied
+     * @param additionalInteractionContract Additional contract address for interaction
+     * @param customData Arbitrary additional data for strategy-specific information
      */
-    function logCloseRateArbitragePosition(
+    function logCloseAgentPosition(
+        uint8 strategyType,
         address collateralAsset,
         address debtAsset,
         uint8 borrowProtocol,
         address borrowMarketId,
         uint256 collateralAmount,
         uint256 debtAmount,
-        address supplyVault
+        address additionalInteractionContract,
+        bytes calldata customData
     ) external {
-        emit CloseRateArbitragePosition(
+        emit CloseAgentPosition(
+            strategyType,
             msg.sender,
             collateralAsset,
             debtAsset,
@@ -87,7 +102,8 @@ contract Logger {
             borrowMarketId,
             collateralAmount,
             debtAmount,
-            supplyVault
+            additionalInteractionContract,
+            customData
         );
     }
 }
